35b9dd8d2f0c8fc3dc47d048b80b8c08db5b43ea
kaung126 <kaungmyat.rgx@gmail.com>
2025-12-03 11:22:16 +0630

    sec

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
-	myslice1 := []int{1, 2, 3}
-	myslice2 := []int{4, 5, 6}
-	myslice3 := append(myslice1, myslice2...)
+	arr1 := [6]int{9, 10, 11, 12, 13, 14} // An array
+	myslice1 := arr1[1:5]                 // Slice array
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 
-	fmt.Printf("myslice3=%v\n", myslice3)
-	fmt.Printf("length=%d\n", len(myslice3))
-	fmt.Printf("capacity=%d\n", cap(myslice3))
+	myslice1 = arr1[1:3] // Change length by re-slicing the array
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21, 22, 23) // Change length by appending items
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 }
